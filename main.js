0586be707912dc10db0cdf17e4fce729cfe57058
Michael <lyrictonka@gmail.com>
2022-05-19 21:39:18 -0500

    trying to merg

--- main.js
+++ main.js
@@ -1,16 +1,61 @@
 'use strict';
 
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+
+
+// test to be made
+//"1234" should give "it works"
+//"1124" shpuld give "it works"
+//"1032" should give "it works"
+//"1528" should give "it works"
+let rampNumArray = []
+const rampNum = (num) => {
+  let numArray = num.toString().split("");
+  for (let i = 0; i < numArray.lenght -1; i++){
+    if (numArray[i] > numArray[i + 1] {
+      return false
+    })
+  }
+  rampNumArray.push(num)
+  return true
+}
+
+
+
+
+console.log(rampNum(1234))
+
+const numOfRamps = (numLimit) => {
+  let counter =0
+  for (let ii = 0; ii < numLimit; ii++){
+    if(rampNum(ii)){
+      counter++
+    }
+  }
+}
+numOfRamps(135)
+
+
+
+
+
+  //for (let i = 0; i < 10000; i++ )
+//const assert = require('assert');
+//const readline = require('readline');
+//const rl = readline.createInterface({
+//  input: process.stdin,
+//  output: process.stdout
+//});
 
 
 
 const rampNum = (str) => {
   console.log('BOOMS', str.length)
+  //loop over string
+  //determine if current item is less than or greater than the last item
+  //if true 
+    //count this  somehow
+
+  //return  
 }
 
 
@@ -23,11 +68,18 @@ const getPrompt = () =>  {
 
 if (typeof describe === 'function') {
   describe('#rampNum()', () => {
-    it('should be of type function', () => {
+    it('should determine if a number is a ramp', () => {
       
       assert.equal(typeof rampNum, "function");
     });
   });
-} else {
+describe ('results', () => {
+  it('should determine how many ramp numbers exist', () => {
+    assert.equal()
+  })
+})
+} 
+
+else {
   getPrompt();
 }
